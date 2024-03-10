2a56b67c2f974197a880c4a38be66c9dc00110ec
arch-fan <juanserracines@gmail.com>
2024-03-10 15:49:30 +0100

    Extend cookie duration

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -3,21 +3,22 @@ import { createToken, cookieName } from "@/lib/jwt";
 import { jres } from "@/utils/response.util";
 
 export const POST: APIRoute = async (ctx) => {
-  const { username, password } = await ctx.request.json();
+	const { username, password } = await ctx.request.json();
 
-  if (
-    username === import.meta.env.USERNAME &&
-    password === import.meta.env.PASSWORD
-  ) {
-    const token = createToken({ username });
+	if (
+		username === import.meta.env.USERNAME &&
+		password === import.meta.env.PASSWORD
+	) {
+		const token = createToken({ username });
 
-    return jres(null, 302, {
-      "Set-Cookie": `${cookieName}=${token}; Path=/; HttpOnly; ${
-        import.meta.env.DEV ? "" : "Secure;"
-      } SameSite=Strict`,
-      Location: "/dashboard",
-    });
-  }
+		const secureRule = import.meta.env.DEV ? "" : "Secure;";
+		const cookieDuration = 60 * 60 * 24 * 30 * 12;
 
-  return jres({ error: "Invalid credentials" }, 401);
+		return jres(null, 302, {
+			"Set-Cookie": `${cookieName}=${token}; Path=/; HttpOnly; SameSite=Strict; Max-Age=${cookieDuration}; ${secureRule}`,
+			Location: "/dashboard",
+		});
+	}
+
+	return jres({ error: "Invalid credentials" }, 401);
 };
